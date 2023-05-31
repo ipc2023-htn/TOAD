d759fd007e9071d7c4cf247464d538310aeeffc6
Daniel Höller <hoeller@cs.uni-saarland.de>
2023-05-31 21:50:04 +0200

    added verify sources

--- verify-plan.sh
+++ verify-plan.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 pfile=$(find . -type f -iname "*.psas")
-/planner/verificationAsPlanning "$pfile" sas_plan >> verify.log
+/planner/verificationAsPlanning verify-all "$pfile" sas_plan >> verify.log
 if [ -f sas_plan.verify ]; then # verifier sometimes shows unsolvability by reachability analysis
-  /planner/pproFF02 sas_plan.verify >> verify.log
-  if ! grep -q "\- Status\: Solved" verify.log; then
-    /planner/dfs05 sas_plan.verify >> verify.log
-    if ! grep -q "\- Status\: Solved" verify.log; then
-      /planner/pproFF1800 sas_plan.verify >> verify.log
-    fi
-  fi
+  /planner/pandaPIengine -g none --heuristic="rc2(add)" sas_plan.verify >> verify.log
+  #if ! grep -q "\- Status\: Solved" verify.log; then
+  #  /planner/dfs05 sas_plan.verify >> verify.log
+  #  if ! grep -q "\- Status\: Solved" verify.log; then
+  #    /planner/pproFF1800 sas_plan.verify >> verify.log
+  #  fi
+  #fi
 fi
 if grep -q "\- Status\: Solved" verify.log; then
   echo "TOAD: verification successful."
